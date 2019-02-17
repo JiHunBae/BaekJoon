24d026f712aa728d276ab29ce20b96d229a4d66e
JiHunBae <qowlgns304@gmail.com>
2019-02-17 21:12:23 +0900

    @2019.02.17 (11718번) 그대로 출력하기 ( 수정 )

--- Main.java
+++ Main.java
@@ -1,11 +1,10 @@
-package newTest;
 import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String copy_String = new String();
-        
+
         while(sc.hasNextLine()) {
             copy_String = sc.next();
             if(!(copy_String.isEmpty() | copy_String.length() > 100))
