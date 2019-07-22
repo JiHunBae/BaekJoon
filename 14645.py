c538d255188bf1bffd6333b87604ae24f0a46d05
unknown <qowlgns304@gmail.com>
2019-07-22 21:25:58 +0900

    @2019.07.22 (14645번) 와이버스 부릉부릉 Upload

---
+++ 14645.py
@@ -0,0 +1,12 @@
+def bus():
+    first_Input = list(map(int, input().split(' ')))
+    number_Of_Stations = first_Input[0]
+
+    temp = ""
+    for i in range(number_Of_Stations):
+        temp = input()
+
+
+    print("비와이")
+    return
+bus()
