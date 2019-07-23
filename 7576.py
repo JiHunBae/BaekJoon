848caea938ed516905008c96edf432c71562ca06
unknown <qowlgns304@gmail.com>
2019-07-23 17:58:15 +0900

    @2019.07.23 (7576번) 토마토(Success) reupload

--- 7576.py
+++ 7576.py
@@ -1,16 +1,14 @@
 from collections import deque
 
-
 def solve():
     input_Value = list(map(int, input().split(' ')))
     number_Of_Columns = input_Value[0]
     number_Of_Rows = input_Value[1]
     queue = deque()
 
-    tomato = [0 for _ in range(number_Of_Rows)]  # 토마토가 익을 최소 날짜 저장 ( 1부터 시작하므로 -1을 반드시 해주고 출력)
-    tomato.append([])
+    tomato = [] # 토마토가 익을 최소 날짜 저장 ( 1부터 시작하므로 -1을 반드시 해주고 출력)
     for index in range(number_Of_Rows):
-        tomato[index] = list(map(int, input().split(' ')))
+        tomato.append(list(map(int, input().split(' '))))
 
     for row in range(number_Of_Rows):
         for col in range(number_Of_Columns):
@@ -32,7 +30,7 @@ def solve():
             if tomato[row_Index][col_Index - 1] is 0:
                 queue.append((row_Index, col_Index - 1))
                 tomato[row_Index][col_Index - 1] = tomato[row_Index][col_Index] + 1
-        if col_Index < (number_Of_Rows - 1):
+        if col_Index < (number_Of_Columns - 1):
             if tomato[row_Index][col_Index + 1] is 0:
                 queue.append((row_Index, col_Index + 1))
                 tomato[row_Index][col_Index + 1] = tomato[row_Index][col_Index] + 1
@@ -46,6 +44,5 @@ def solve():
             max_Value = max(max_Value, tomato[row][col])
 
     print(max_Value - 1)
-
 
 solve()
