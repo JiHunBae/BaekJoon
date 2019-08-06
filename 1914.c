5667e014991f33423ac4bb63f0ded3b0aeb72c64
unknown <qowlgns304@gmail.com>
2019-08-06 18:30:19 +0900

    @2019.08.06 (1914번) 하노이 탑(코드 작성 중) Upload

--- 1914.c
+++ 1914.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-#define BIGINT_MAXSIZE 40
+#define BIGINT_MAXSIZE 80
 void move_Hanoi(int number_Of_Circles, int move_From, int move_By, int move_To);
+int* bigint_Multiplication(int *first_Factor,int first_Size, int *second_Factor, int second_Size, int *result);
 void bigint_Print(int *number);
 int main() {
     int number_Of_Total_Circles;
@@ -9,11 +10,15 @@ int main() {
     char operator;
     int result[BIGINT_MAXSIZE] = { - 1 };
     int calc_Temp[40] = { - 1 };
-
+    int a[40] = { 0 };
+    int b[40] = { 0 };
+    printf("%d\n", sizeof(result) / sizeof(int));
     scanf("%d", &number_Of_Total_Circles);
-
+    a[0] = 4;
+    a[1] = 2;
+    b[0] = 3;
     if(number_Of_Total_Circles > 20) {
-
+        bigint_Multiplication(a, sizeof(a)/sizeof(a[0]), b, sizeof(b)/sizeof(b[0]), result);
     } else {
         move_Hanoi(number_Of_Total_Circles, 1, 2, 3);
     }
@@ -37,21 +42,27 @@ void bigint_Print(int *number) {
     }
 }
 
-int *bigint_Multiplication(int *first_Factor, int *second_Factor) {
-    int first_Factor_Size = sizeof(first_Factor) / sizeof(int);
-    int second_Factor_Size = sizeof(second_Factor) / sizeof(int);
-    int multiplicatin_Result[BIGINT_MAXSIZE] =  { 0 };
+int *bigint_Multiplication(int *first_Factor, int first_Size, int *second_Factor, int second_Size, int *result) {
+    int multiplication_Result[BIGINT_MAXSIZE] = { 0 };
     int temp_Of_Mul_Each_Position, position_Of_First_Factor, position_Of_Second_Factor;
     int mul_Value_Current_Position, mul_Value_Next_Position;
+    int temp_For_Plus;
 
-    for(position_Of_First_Factor = 0; position_Of_First_Factor < first_Factor_Size; ++position_Of_First_Factor) {
-        for(position_Of_Second_Factor = 0; position_Of_Second_Factor < second_Factor_Size; ++position_Of_Second_Factor) {
+    for(position_Of_First_Factor = 0; position_Of_First_Factor < first_Size; ++position_Of_First_Factor) {
+        for(position_Of_Second_Factor = 0; position_Of_Second_Factor < second_Size; ++position_Of_Second_Factor) {
             temp_Of_Mul_Each_Position = first_Factor[position_Of_First_Factor] * second_Factor[position_Of_Second_Factor];
             if(temp_Of_Mul_Each_Position > 9) {
                 mul_Value_Current_Position = temp_Of_Mul_Each_Position % 10;
                 mul_Value_Next_Position = temp_Of_Mul_Each_Position - mul_Value_Current_Position;
                 mul_Value_Next_Position = mul_Value_Next_Position / 10;
-                multiplicatin_Result[position_Of_First_Factor + position_Of_Second_Factor] =
+                temp_For_Plus = multiplication_Result[position_Of_First_Factor + position_Of_Second_Factor] + mul_Value_Current_Position;
+                multiplication_Result[position_Of_First_Factor + position_Of_Second_Factor] = temp_For_Plus % 10;
+                multiplication_Result[position_Of_First_Factor + position_Of_Second_Factor + 1] = (temp_For_Plus / 10) + mul_Value_Next_Position;
+            } else {
+                mul_Value_Current_Position = temp_Of_Mul_Each_Position;
+                temp_For_Plus = multiplication_Result[position_Of_First_Factor + position_Of_Second_Factor] + mul_Value_Current_Position;
+                multiplication_Result[position_Of_First_Factor + position_Of_Second_Factor] = temp_For_Plus % 10;
+                multiplication_Result[position_Of_First_Factor + position_Of_Second_Factor + 1] = temp_For_Plus / 10;
             }
         }
     }
